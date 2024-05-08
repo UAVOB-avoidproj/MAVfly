eedc689ecb30700bfd37bb8a2cae049e49e98745
huanzze <2805033624@qq.com>
2024-05-08 15:26:32 +0800

    Refactoring code structure

--- lib/build/CMakeFiles/mavfly.dir/cmake_clean.cmake
+++ lib/build/CMakeFiles/mavfly.dir/cmake_clean.cmake
@@ -1,8 +1,8 @@
 file(REMOVE_RECURSE
+  "../libmavfly.pdb"
+  "../libmavfly.so"
   "CMakeFiles/mavfly.dir/mavsdk_simplify.cpp.o"
   "CMakeFiles/mavfly.dir/mavsdk_simplify.cpp.o.d"
-  "libmavfly.pdb"
-  "libmavfly.so"
 )
 
 # Per-language clean rules from dependency scanning.
