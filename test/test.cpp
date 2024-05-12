3b956208f578ad93f30f06a91ae4eadd9b363921
huanzze <2805033624@qq.com>
2024-05-12 09:32:52 +0800

    Add subscribe_Global_Position

--- test/test.cpp
+++ test/test.cpp
@@ -33,69 +33,69 @@ int main(){
 
     if(!Armed()){return 0;}
 
-    // //  action 控制起飞
-    // rt = 0;
-    // std::thread thread_takeoff(Action_takeoff, &rt, 3);
-    // thread_takeoff.detach();
-    // while(1){
-    //     if(rt == 0) std::cout << "Action_takeoff ……\n";
-    //     if(rt == 1) break;
-    //     if(rt == -1) {std::cerr << "Action_takeoff failed\n";return 0;}
-    //     sleep_for(seconds(1));
-    // }
-    // std::cout << "Action_takeoff successful!\n";
+    //  action 控制起飞
+    rt = 0;
+    std::thread thread_takeoff(Action_takeoff, &rt, 3);
+    thread_takeoff.detach();
+    while(1){
+        if(rt == 0) std::cout << "Action_takeoff ……\n";
+        if(rt == 1) break;
+        if(rt == -1) {std::cerr << "Action_takeoff failed\n";return 0;}
+        sleep_for(seconds(1));
+    }
+    std::cout << "Action_takeoff successful!\n";
 
 
+
+    // 等候 5 秒
+    sleep_for(seconds(30));
+
+
+    // 初始化 offboard 控制
+    rt = 0;
+    std::thread thread2(Offboard_Init, &rt, wait_time);
+    thread2.detach();
+    while(1){
+        if(rt == 0) std::cout << "offboard_init ……\n";
+        if(rt == 1) break;
+        if(rt == -1) {std::cerr << "offboard_init failed\n";return 0;}
+        sleep_for(seconds(1));
+    }
+    std::cout << "offboard_init successful!\n";
+
+    // // // 设置 offboard 飞行姿态为“向前”
+    // setOffboard_VBY(vbyForward);
 
     // // 等候 5 秒
-    // sleep_for(seconds(30));
+    // sleep_for(seconds(10));
+    // setOffboard_VBY(vbyStop);
 
+    // action 控制降落
+    rt = 0;
+    std::thread thread_land(Action_land, &rt);
+    thread_land.detach();
+    while(1){
+        if(rt == 0) std::cout << "Action_land ……\n";
+        if(rt == 1) break;
+        if(rt == -1) {std::cerr << "Action_land failed\n";return 0;}
+        sleep_for(seconds(1));
+    }
+    std::cout << "Action_land successful!\n";
 
-    // // 初始化 offboard 控制
-    // rt = 0;
-    // std::thread thread2(Offboard_Init, &rt, wait_time);
-    // thread2.detach();
-    // while(1){
-    //     if(rt == 0) std::cout << "offboard_init ……\n";
-    //     if(rt == 1) break;
-    //     if(rt == -1) {std::cerr << "offboard_init failed\n";return 0;}
-    //     sleep_for(seconds(1));
-    // }
-    // std::cout << "offboard_init successful!\n";
+    // 停止 offboard 控制
+    rt = 0;
+    std::thread thread3(Offboard_Stop, &rt);
+    thread3.detach();
+    while(1){
+        if(rt == 0) std::cout << "offboard_stop ……\n";
+        if(rt == 1) break;
+        if(rt == -1) {std::cerr << "offboard_stop failed\n";return 0;}
+        sleep_for(seconds(1));
+    }
+    std::cout << "offboard_stop successful!\n";
 
-    // // // // 设置 offboard 飞行姿态为“向前”
-    // // setOffboard_VBY(vbyForward);
-
-    // // // 等候 5 秒
-    // // sleep_for(seconds(10));
-    // // setOffboard_VBY(vbyStop);
-
-    // // action 控制降落
-    // rt = 0;
-    // std::thread thread_land(Action_land, &rt);
-    // thread_land.detach();
-    // while(1){
-    //     if(rt == 0) std::cout << "Action_land ……\n";
-    //     if(rt == 1) break;
-    //     if(rt == -1) {std::cerr << "Action_land failed\n";return 0;}
-    //     sleep_for(seconds(1));
-    // }
-    // std::cout << "Action_land successful!\n";
-
-    // // 停止 offboard 控制
-    // rt = 0;
-    // std::thread thread3(Offboard_Stop, &rt);
-    // thread3.detach();
-    // while(1){
-    //     if(rt == 0) std::cout << "offboard_stop ……\n";
-    //     if(rt == 1) break;
-    //     if(rt == -1) {std::cerr << "offboard_stop failed\n";return 0;}
-    //     sleep_for(seconds(1));
-    // }
-    // std::cout << "offboard_stop successful!\n";
-
-    // while(1){
-    //     sleep_for(seconds(5));
-    // }
+    while(1){
+        sleep_for(seconds(5));
+    }
     return 0;
 }
